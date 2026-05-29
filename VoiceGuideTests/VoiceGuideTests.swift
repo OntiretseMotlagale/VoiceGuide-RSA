61acb8581656cf17c3b64d60afdd45705fd9967a
ontiretsemmotlagale <ontiretsemotlagale@gmail.com>
2026-05-29 11:20:45 +0200

    Added a feature build

---
+++ VoiceGuideTests/VoiceGuideTests.swift
@@ -0,0 +1,16 @@
+//
+//  VoiceGuideTests.swift
+//  VoiceGuideTests
+//
+//  Created by Ontiretse Motlagale on 2026/05/28.
+//
+
+import Testing
+
+struct VoiceGuideTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
